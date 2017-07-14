5cbba09981da38cee9d45f472ece13ae93697dd9
69myluckynumber <muravyoov@gmail.com>
2017-07-14 21:45:55 +0300

    Initial commit

---
+++ MoodPocket.WebUI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MoodPocket.WebUI
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ MoodPocket.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoodPocket.WebUI.MvcApplication" Language="C#" %>
